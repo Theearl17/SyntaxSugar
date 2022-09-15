2cef6781340bdddd3c325bb23e0e88ab8b514202
earl starks <earlstarks@rocketmail.com>
2022-09-15 12:06:30 -0500

    SyntaxSugar

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,17 @@
+﻿//var firstName = 7;
+//var lastName = "Starks";
+//if ({firstName} == Earl && {lastName} == Starks )
+//{
+//    Console.WriteLine(" User is a member of program.");
+
+//}
+
+
+
+#region 
+// Exercise 1 Instructions
+var answer = 4;
+var response= (answer < 9) ? $" {answer} is less than nine" : $" {answer} is greater than or equal to nine";
+Console.WriteLine(response);
+#endregion
+
